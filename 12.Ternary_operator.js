e194fd7668c12f9a3679f4fb4aeb40902a43cfce
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-08 21:13:02 +0600

    Update 12.Ternary_operator.js

---
+++ 12.Ternary_operator.js
@@ -0,0 +1,14 @@
+var n = 1
+var str = ''
+
+if (n%2 ==0){
+    str = 'EVEN'
+}
+else{
+    str = 'ODD'
+}
+console.log(str)
+
+var result = n%2 == 0 ? "EVEN" : "ODD"
+//condition ? true side : false side
+console.log(result)
