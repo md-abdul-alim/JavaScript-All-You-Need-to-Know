4cb7b850930037d00e6ac6f6484a3b4c186b1f66
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-08 21:20:57 +0600

    Update 13.And_Or_Shorthand.js

---
+++ 13.And_Or_Shorthand.js
@@ -0,0 +1,15 @@
+var name = 'Abdul Alim'
+
+var fullname = name || "Milon"
+console.log(fullname)
+
+var name = ''
+
+var fullname = name || "Milon"
+console.log(fullname)
+
+var isOK = false
+isOK && console.log("Everything is OK")
+
+var isOK = true
+isOK && console.log("Everything is OK")
