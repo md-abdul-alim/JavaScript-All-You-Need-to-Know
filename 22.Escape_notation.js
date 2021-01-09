6b1809e80010f26192981fbeb392da18627be563
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-09 17:00:16 +0600

    update 22.Escape_notation.js

---
+++ 22.Escape_notation.js
@@ -0,0 +1,12 @@
+
+var str = 'This is a \'String\''
+console.log(str)
+
+var str = 'This is a \nString'
+console.log(str)
+
+var str = 'This is a \tString'
+console.log(str)
+
+var str = 'This is a \\String'
+console.log(str)
