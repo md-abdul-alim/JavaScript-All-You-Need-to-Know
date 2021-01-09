773fd9ac8c001af962f2f8f6a71621bf9391bda1
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-09 16:40:40 +0600

    Update 20.Infinity_for_loop.js

---
+++ 20.Infinity_for_loop.js
@@ -0,0 +1,10 @@
+
+for(; ;){
+    var rand = Math.floor(Math.random() * 10 +1)
+    if (rand ==9){
+        console.log("Whinner winner chicken Dinner")
+        break
+    }else{
+        console.log("You have got " + rand)
+    }
+}
