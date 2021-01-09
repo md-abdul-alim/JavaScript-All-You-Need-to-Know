27e1ea2ab29405a06684f03eb5ff6a685c139a50
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-09 16:48:50 +0600

    Update 21.String_literal_VS_Constructor.js

---
+++ 21.String_literal_VS_Constructor.js
@@ -0,0 +1,17 @@
+var str = 'String Literal' // Literal method of defining string
+console.log(str)
+var str2 = String("String Constructor")
+console.log(str2)
+
+// converting number to string
+ var n =10
+ var str3 = n
+ console.log("Number",str3)
+ var str4 = n +''
+ console.log("String number", str4)
+
+var str5 = n.toString()
+console.log("String number", str5)
+
+var str6 = String(n)
+console.log("String number", str6)
