c5af02bd2ece70337ed2d703938b69dfcf18e4c7
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-09 18:09:53 +0600

    Update 26.Array_declaration.js

---
+++ 26.Array_declaration.js
@@ -0,0 +1,19 @@
+var arr= [1,2,3,4,5,6]
+console.log(arr)
+
+console.log(arr[4])
+
+arr[10] = 11
+
+console.log(arr)
+
+console.log(arr[8])
+console.log(arr.length)
+
+arr[2] = 33
+console.log(arr)
+
+//shortcut way
+
+var arr2 = Array(1,2,3,4)
+console.log(arr2)
