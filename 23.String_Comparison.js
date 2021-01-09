15b866563df7d4f76c3100cfa3408232fdabd149
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-09 17:06:36 +0600

    Update 23.String_Comparison.js

---
+++ 23.String_Comparison.js
@@ -0,0 +1,19 @@
+var a = 'abc'
+var b = 'def'
+
+console.log(a == b)
+console.log(a < b)
+
+console.log('a' < 'b')
+
+console.log('z' < 'Z')
+console.log('a' < 'z')
+
+var a = 'abc'
+var b = 'aef'
+
+console.log(a < b)
+
+// number ke string kore compare korar somoy string ke number a convert kore fele
+console.log('001' ==2)
+console.log('001' ==1)
