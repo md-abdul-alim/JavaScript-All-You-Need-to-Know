3a4e2751f03cf2412533f497c158619d38ffc3c1
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-19 23:10:05 +0600

    Update 43.Closure.js

--- 43.Closure.js
+++ 43.Closure.js
@@ -1,7 +1,18 @@
 //Closure is when a function is able to remember and access it's lexical scope even
 // when that function executing outside it's lexical scope
 
+
 //Example 1
+function test(){
+    var msg = 'this is a closure'
+    function sayMsg(){
+        console.log(msg)
+    }
+    sayMsg()
+}
+test()
+console.log("------------------")
+//Example 2
 var b = 10
 
 function a(){
@@ -12,5 +23,24 @@ function a(){
 }
 var abc = a()
 console.dir(abc)
-
-//Example 2
+abc() //calling outside it's scope
+console.log("------------------")
+//Example 3
+//step 1
+// for(var m = 1; m<=5;m++){
+//     //here answer will be 6 five times. because loop is running first.
+//     //than after completing loop 5 times when m=6 and the loop is end.
+//     //than the setTimeout function is calling
+//     setTimeout(function(){
+//         console.log(m)
+//     },1000*m)
+// }
+//step 2
+//to solve this last timeout calling
+for(var m = 1; m<=5;m++){
+    (function(n){
+        setTimeout(function(){
+            console.log(n)
+        },1000*n)
+    })(m)
+}
