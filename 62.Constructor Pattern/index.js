f23d6d253385300ec72124fee9c025865884d595
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-22 15:18:39 +0600

    Constructor Pattern

---
+++ 62.Constructor Pattern/index.js
@@ -0,0 +1,32 @@
+//Constructor pattern e object variable capital letter dia suru hobe
+//sob variable & method this dia call korte hobe
+
+var Rectangle = function(width, height){
+    this.width = width
+    this.height = height
+
+    this.draw = function(){
+        console.log("This is a rectangle")
+        this.printProperties()
+        console.log(this)
+    }
+    this.printProperties = function(){
+        console.log("Width is "+ this.width)
+        console.log("height is "+ this.height)
+    }
+ }
+
+// var rect3 = new Rectangle(55,99)
+// rect3.draw()
+
+function myNew(constructor){
+    var obj = {}
+    Object.setPrototypeOf(obj, constructor.prototype)
+    var argsArray = Array.prototype.slice.apply(arguments)
+    constructor.apply(obj, argsArray.slice(1))
+
+    return obj
+}
+
+var rect4 = myNew(Rectangle, 45,30)
+rect4.draw()
