f1d43a485037c1cdc53deb33228dd842dce9dc05
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-23 20:08:17 +0600

    Update instance prototype

--- 73.Instance vs Prototype/index.js
+++ 73.Instance vs Prototype/index.js
@@ -2,6 +2,10 @@ function Square(width){
     //ekane constructor jegula declare kora hobe hobe egula 
     //instance member
     this.width = width
+    this.getWidth = function(){
+        console.log('Width as a instance member: ' + this.width)
+        this.draw()
+    }
     // this.draw = function(){
     //     console.log('Draw')
     // }
@@ -11,10 +15,11 @@ function Square(width){
 Square.prototype = {
     //prototype member
     draw: function(){
-        console.log('Draw')
+        //this.getWidth()
+        console.log('Draw is a property member')
     },
     toString: function(){
-        return "My width is = " + this.width
+        return "Width as a prototype member: " + this.width
     }
 }
 var sqr1 = new Square(10)
@@ -22,3 +27,7 @@ var sqr2 = new Square(5)
 
 console.log(sqr1.draw())
 console.log(sqr2.toString())
+console.log(sqr1.getWidth())
+
+//Note: instance member can access the prototype member.
+//As well as prototype member can also access the instance member.
